ccf31cb33e40cdc6d58bbdfcf2b96c496d27c559
wistox07 <fodelosrios@gmail.com>
2022-10-04 03:01:54 -0500

    oracle

---
+++ ConexionOracle.php
@@ -0,0 +1,68 @@
+<?php
+/*
+         class Conexion {
+              public function Conectar (){
+                   define ('HOST' , '127.0.0.1' );
+                   define ('PORT' , 1521);
+                   define ('NAME' , 'XE');
+                   define ('USER' , 'proyecto');
+                   define ('PASS' , 'proyectoasd');
+                   $bd_settings = "(DESCRIPTION =
+                        (ADDRESS =
+                        )
+                             (PROTOCOL = TCP)
+                             (HOST = " .HOST . " )
+                             (PORT = " .PORT . ")
+                        )
+                        (CONNECT_DATA =
+                        (SERVER = DEDICATED)
+                        (SERVICE_NAME = " .NAME. ")
+                        )
+                   )";
+                   try {
+                        $bd = new PDO( 'oci : dbname =' .$bd_settings , USER , PASS) ;
+                        $bd->setAttribute (PDO ::ATTR_CASE , PDO ::CASE_LOWER) ;
+                        $bd->setAttribute (PDO ::ATTR_ERRMODE , PDO ::ERRMODE_EXCEPTION) ;
+                        echo "CONEXION EXITOSA";
+                        return $bd;
+                   }catch (Exception $e){
+                        echo "ERROR DE CONEXION : .$e->getMessage();
+                   }
+                   
+                }
+            }
+            */
+
+class Conexion{
+    public function Conectar(){
+        define ('HOST' , '127.0.0.1' );
+        define ('PORT' , 1521);
+        define ('NAME' , 'XE');
+        define ('USER' , 'proyecto');
+        define ('PASS' , 'proyectoasd');
+        $bd_settings = "(DESCRIPTION =
+                        (ADDRESS =
+                            (PROTOCOL = TCP)
+                            (HOST = " .HOST .")
+                            (PORT = " .PORT .")
+                        )
+                        (CONNECT_DATA =
+                        (SERVER = DEDICATED)
+                        (SERVICE_NAME = " .NAME. ")
+                        )
+                   )";
+        try{
+            $bd = new PDO( 'oci : dbname =' .$bd_settings , USER , PASS) ;
+                        $bd->setAttribute (PDO ::ATTR_CASE , PDO ::CASE_LOWER) ;
+                        $bd->setAttribute (PDO ::ATTR_ERRMODE , PDO ::ERRMODE_EXCEPTION) ;
+                        echo "CONEXION EXITOSA";
+                        return $bd;
+        }
+        catch(Exception $e){
+            echo "ERROR DE CONEXION : ".$e->getMessage();
+        }
+
+        
+    }
+}
+?>
